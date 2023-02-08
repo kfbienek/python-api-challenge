16fa52e5f4a48138036d3ddd955b3cf47ff3a9d8
kfbienek <kfbienek@utexas.edu>
2023-02-08 15:52:01 -0600

    my message

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2590e9e99d615b1c5a4c763c2192ab4e"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "1f7d51582bb24db199bef25443b3baf1"
